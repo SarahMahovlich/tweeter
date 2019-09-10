5f9118895baef8832f9cbbef571617ebf46373d9
Sarah <sarah.mahov@gmail.com>
2019-09-10 22:57:31 +0000

    add char counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log('it works');
+
+  $("#target").on('input', function() {
+    let counter = 140 - this.value.length;
+    console.log(counter);
+
+    if (counter >= 0) {
+      $('#counter').text(counter).removeClass("counterRed");
+    } else if (counter <= 0) {
+      $("#counter").text(counter).addClass("counterRed");
+    }
+
+  });
+
+});
+
